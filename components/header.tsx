1736d3ce90d43650010f757563f1ee21ec46f33f
fogelmark <108455196+fogelmark@users.noreply.github.com>
2024-09-27 12:17:22 +0200

    Refactored header component

--- components/header.tsx
+++ components/header.tsx
@@ -5,6 +5,14 @@ import { archivo_black } from "@/lib/fonts"
 import { Spiral as Hamburger } from "hamburger-react"
 import { useMediaQuery } from "usehooks-ts"
 import { useOpenContext } from "@/lib/hooks"
+import { cn } from "@/lib/utils"
+
+const fadeInOut = {
+  initial: { opacity: 0 },
+  animate: { opacity: 1 },
+  exit: { opacity: 0 },
+  transition: { duration: 0.3 },
+}
 
 export function Header() {
   const { scrollY } = useScroll()
@@ -41,43 +49,43 @@ export function Header() {
       className={classes.header}
     >
       <motion.span
+        {...fadeInOut}
         style={{ color: textColor }}
-        className={`${archivo_black.className} text-4xl -tracking-widest`}
+        className={cn(archivo_black.className, "text-4xl -tracking-widest")}
       >
         brand.
       </motion.span>
 
       <motion.div
-        className={`${isOpen ? "!text-black" : ""} z-10 hidden max-md:block`}
+        className={cn("z-10 hidden max-md:block", { "!text-black": isOpen })}
         style={{ color: textColor }}
       >
         <Hamburger toggled={isOpen} toggle={setOpen} />
       </motion.div>
 
       <motion.nav
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        exit={{ opacity: 0 }}
-        className={`${isOpen ? "fixed inset-0 flex h-full w-full items-center justify-center bg-white" : "max-md:hidden"}`}
+        {...fadeInOut}
+        className={cn({
+          "fixed inset-0 flex h-full w-full items-center justify-center bg-white":
+            isOpen,
+          "max-md:hidden": !isOpen,
+        })}
       >
         <ul
-          className={`${isOpen ? "flex flex-col items-center gap-4 text-4xl" : "flex gap-4"}`}
+          className={cn({
+            "flex flex-col items-center gap-4 text-4xl": isOpen,
+            "flex gap-4": !isOpen,
+          })}
         >
-          <motion.li
-            style={{ color: textColor }}
-            className={`${isOpen ? "!text-black" : ""} ${classes.li}`}
-            children="Home"
-          />
-          <motion.li
-            style={{ color: textColor }}
-            className={`${isOpen ? "!text-black" : ""} ${classes.li}`}
-            children="About"
-          />
-          <motion.li
-            style={{ color: textColor }}
-            className={`${isOpen ? "!text-black" : ""} ${classes.li}`}
-            children="Contact"
-          />
+          {["Home", "About", "Contact"].map((item) => (
+            <motion.li
+              key={item}
+              style={{ color: textColor }}
+              className={cn(classes.li, { "!text-black": isOpen })}
+            >
+              {item}
+            </motion.li>
+          ))}
         </ul>
       </motion.nav>
     </motion.header>
